a6e1cf0dbb02dd95615635d2282a7eefc8216a22
solomonp90 <solomonraulpena@gmail.com>
2019-10-27 22:56:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let thankyoumessages = []
+function writeCards(names,events){
+    for (let i = 0; i < names.length; i++) {
+        thankyoumessages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return thankyoumessages
+}
+
+function countDown(){
+    let countup = 0;
+    while (countup < 11) {
+      console.log(countup++);
+    }
+}
+
+// 'Thank you, Lisa, for the wonderful surprise gift!'
